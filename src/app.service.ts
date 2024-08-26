5ca3bd047ae231ea66852f96712ada478cb5dcd6
aya <ayoob.khodadadi@gmail.com>
2024-08-26 19:45:20 +0330

    feat(logs): new exmple log for adding span id to log

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { logger } from './tracing';
+import { LoggerWrapperObj, getSpanInfo } from './tracing';
 import { SeverityNumber } from "@opentelemetry/api-logs";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    logger.emit({
+    LoggerWrapperObj.emit({
       severityNumber: SeverityNumber.INFO,
       severityText: "info",
-      body: { type: 'log', name: 'startNestJs' , created_at: Math.floor(new Date().getTime() / 1000) },
-      attributes: { type: 'log', name: 'startNestJs' , created_at: Math.floor(new Date().getTime() / 1000) },
+      body: {
+        type: 'log',
+        name: 'startNestJs',
+        created_at: Math.floor(new Date().getTime() / 1000)
+      },
+      attributes: {
+        type: 'log',
+        name: 'startNestJs',
+        created_at: Math.floor(new Date().getTime() / 1000)
+      },
     })
     return 'Hello World!';
   }
--- src/tracing.ts
+++ src/tracing.ts
@@ -10,8 +10,11 @@ import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
-import { BatchLogRecordProcessor, LoggerProvider } from '@opentelemetry/sdk-logs';
+import { BatchLogRecordProcessor, LogRecord, LoggerProvider } from '@opentelemetry/sdk-logs';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+import api from '@opentelemetry/api';
+import type * as logsAPI from '@opentelemetry/api-logs';
+import { SeverityNumber } from "@opentelemetry/api-logs";
 
 const serviceName = 'NestJs-service';
 const serviceVersion = '0.1.1';
@@ -19,7 +22,7 @@ const serviceVersion = '0.1.1';
 
 export function setupTracing() {
   const consoleExporter: boolean = false;
-  let traceExporter,metricReader;
+  let traceExporter;
 
   if (consoleExporter) {
     traceExporter = new ConsoleSpanExporter();
@@ -40,7 +43,7 @@ export function setupTracing() {
         headers: {},
       }),
     }),
-  
+
     instrumentations: [
       new NestInstrumentation(),
       new HttpInstrumentation(),
@@ -51,22 +54,61 @@ export function setupTracing() {
   sdk.start();
 }
 
-const logResource = Resource.default().merge(
-  new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: serviceName,
-    [SEMRESATTRS_SERVICE_VERSION]: serviceVersion,
-  }),
-);
 
-const loggerProvider = new LoggerProvider({
-  resource: logResource,
-});
+//  const logger = loggerProvider.getLogger('default', '1.0.0');
 
-const logExporter = new OTLPLogExporter({
-  url: 'http://127.0.0.1:4318/v1/logs',
-});
+export const getSpanInfo = () => {
+  return api.trace.getSpan(api.context.active());
+}
 
+class LoggerWrapper {
+  private logger: logsAPI.Logger;
 
-loggerProvider.addLogRecordProcessor(new BatchLogRecordProcessor(logExporter));
+  constructor() {
+    const logResource = Resource.default().merge(
+      new Resource({
+        [SEMRESATTRS_SERVICE_NAME]: serviceName,
+        [SEMRESATTRS_SERVICE_VERSION]: serviceVersion,
+      }),
+    );
 
-export const logger = loggerProvider.getLogger('default', '1.0.0');
+    const loggerProvider = new LoggerProvider({
+      resource: logResource,
+    });
+
+    const logExporter = new OTLPLogExporter({
+      url: 'http://127.0.0.1:4318/v1/logs',
+    });
+
+    loggerProvider.addLogRecordProcessor(new BatchLogRecordProcessor(logExporter));
+    this.logger = loggerProvider.getLogger('default', '1.0.0');
+  }
+
+  emit(logRecord: {
+    severityNumber: SeverityNumber,
+    severityText: string,
+    attributes: Record<string, any>,
+    body: Record<string, any>
+  }) {
+    const current_span = getSpanInfo()
+
+    let trace_id = current_span.spanContext().traceId;
+    let span_id = current_span.spanContext().spanId;
+
+    logRecord.attributes = {
+      ...logRecord.attributes,
+      trace_id,
+      span_id,
+    }
+
+    logRecord.body = {
+      ...logRecord.body,
+      trace_id,
+      span_id,
+    }
+
+    this.logger.emit(logRecord as unknown as LogRecord);
+  }
+}
+
+export const LoggerWrapperObj = new LoggerWrapper();
